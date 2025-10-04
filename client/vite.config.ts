601a70f23e0cc7ad4ebd5e1c62e01cc1825d9ab6
epot <epot@confluent.io>
2025-10-04 15:14:19 +0200

    move stuff around

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  build : {
+    outDir: "../dist",
+    emptyOutDir: true
+  },
   server:
     mode === "development"
       ? {
